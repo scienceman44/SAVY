0b5d16d05fbe65fd88d0b7a506c9a0ea56076a6d
scienceman44 <hmptn37215@gmail.com>
2017-07-10 15:32:46 -0500

    Add files via upload

--- proj01/proj01.py
+++ proj01/proj01.py
@@ -1,4 +1,4 @@
-# Name:
+# Name:aaron
 # Date:
 
 # proj01: A Simple Program
@@ -6,5 +6,19 @@
 # Then, it prints a sentence that says when the user will turn 100.
 
 # If you complete extensions, describe your extensions here!
-
-
+y = raw_input('enter your name:')
+b = raw_input('enter your age:')
+x = raw_input('enter the year:')
+l = raw_input('have you had a birthday yet this year?:')
+result = float(x) - float(b) + 100
+if l == 'yes':
+    result = result+0
+elif l == 'no':
+    result = result-1
+print (y), 'will turn 100 in the year', result
+if int(b) < 13:
+    print "you can watch G and PG rated movies "
+elif int(b) <= 17:
+    print "you can see G , PG, and PG-13 Rated movies"
+elif int(b) >= 18:
+    print "you can see G, PG, PG-13, and R rated movies "
