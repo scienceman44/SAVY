f7bc39a1874d30bb8b9c635cfee1c199b96541aa
scienceman44 <hmptn37215@gmail.com>
2017-07-11 14:27:01 -0500

    Add files via upload

--- proj03/proj03.py
+++ proj03/proj03.py
@@ -11,5 +11,20 @@ or exactly right. Keep the game going until the user types exit.
 Keep track of how many guesses the user has taken, and when the game ends, print this out.
 
 """
-
+Guesses = 5
+import random
+x = True
+answer = random.randint(1,9)
+while x == True:
+    n = raw_input('I am thinking of a number 1 to 9 can you guess it?')
+    if answer != n:
+        Guesses = Guesses - 1
+        print 'look out you only have' ,Guesses, 'gueses left!!'
+    while x == True:
+        if Guesses == 0:
+            print 'you ran out of lives :('
+        x = False
+    elif answer == n:
+    print 'GOOD JOB YOU GUESED CORECTILY'
+        x = False
 
