e3490cf735daf2f6c7c90bb79f9cad7025718e3c
scienceman44 <hmptn37215@gmail.com>
2017-07-11 11:23:20 -0500

    Add files via upload

--- proj02/proj02_01.py
+++ proj02/proj02_01.py
@@ -1,19 +1,14 @@
 # Name:
 # Date:
 
-# proj02: sum
+    result = 0
+    loop_control = True
+while loop_control == True:
+    b = int(raw_input('Enter a number to add, or 0 to indicate you are finished:'))
+    if b == 0:
+        loop_control = False
+    result = result + b
+print 'your result is:'
+print result
 
-# Write a program that prompts the user to enter numbers, one per line,
-# ending with a line containing 0, and keep a running sum of the numbers.
-# Only print out the sum after all the numbers are entered
-# (at least in your final version). Each time you read in a number,
-# you can immediately use it for your sum,
-# and then be done with the number just entered.
 
-#Example:
-# Enter a number to sum, or 0 to indicate you are finished: 4
-# Enter a number to sum, or 0 to indicate you are finished: 5
-# Enter a number to sum, or 0 to indicate you are finished: 2
-# Enter a number to sum, or 0 to indicate you are finished: 10
-# Enter a number to sum, or 0 to indicate you are finished: 0
-#The sum of your numbers is: 21
--- proj02/proj02_02.py
+++ proj02/proj02_02.py
@@ -9,5 +9,9 @@ sequence is a sequence of numbers where the next number in the sequence is the s
 previous two numbers in the sequence. The sequence looks like this: 
 1, 1, 2, 3, 5, 8, 13...
 """
+x = 0
+x = raw_input("up to witch number in the Fibonacci sequence yould you like me to generate? " "up to witch number in the Fibonacci sequence yould you like me to generate? "
+
+while x > 0:
 
 
